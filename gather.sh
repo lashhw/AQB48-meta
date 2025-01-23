d6cd58c04fbabe055602004ed3a9e6c691abfb8f
Yen-Chieh Huang <71396086+lashhw@users.noreply.github.com>
2025-01-23 14:39:04 +0800

    Update gather.sh

--- gather.sh
+++ gather.sh
@@ -3,7 +3,7 @@ set -e
 
 for i in bathroom bmw classroom house kitchen staircase teapot; do
     cd AQB48-baseline-catapult/data
-    make clean
+    rm -rf -- *.bin
     ./generate "../../scene/${i}.ply" "../../scene-small/${i}_100000.ray"
 
     cd ../work
